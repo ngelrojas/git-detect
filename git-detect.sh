485fbb5815f929cc026d3f611801c6c9d1855973
ngel rojas <aomarrojasp@gmail.com>
2020-06-29 10:38:51 -0300

    rename file

---
+++ git-detect.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# display files affected
+echo "changes on files $(git ls-files -m)"
+
+# save names files in var NAME_FILES
+NAME_FILES=$(git status --porcelain|tr '\n' ' ')
+
+# add all files affected
+git add .
+
+# commit with all files with each name affected
+git commit -m "your files affected: ${NAME_FILES}"
+
+# message ready to push
+echo "your files are commited, ready to push...!"
